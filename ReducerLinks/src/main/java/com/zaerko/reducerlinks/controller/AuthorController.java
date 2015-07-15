57310257276d227a2dff219da35db98228570149
Zaerko_DV <zaerko1991@gmail.com>
2015-07-15 19:27:23 +0200

    Rebuild progect.Add work version.

--- ReducerLinks/src/main/java/com/zaerko/reducerlinks/controller/AuthorController.java
+++ ReducerLinks/src/main/java/com/zaerko/reducerlinks/controller/AuthorController.java
@@ -15,49 +15,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.zaerko.reducerlinks.model.Author;
-import com.zaerko.reducerlinks.service.IAuthorService;
+//import com.zaerko.reducerlinks.model.Author;
+//import com.zaerko.reducerlinks.service.IAuthorService;
 
 /**
  * @author Zaerko_DV
  *
  */
-@Controller
-@RequestMapping("/author")
+//@Controller
+//@RequestMapping("/author")
 public class AuthorController {
-
-	@Inject
-	private IAuthorService authorService;
-
-	private static final Logger logger = LoggerFactory.getLogger(AuthorController.class);
-
-	@RequestMapping(value="/signup", method=RequestMethod.GET)
-	public ModelAndView signup(ModelMap model) {			
-		return new ModelAndView();
-	}
-
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public @ResponseBody String operationStatus(@RequestBody Author author) {
-
-		authorService.addAuthor(author);
-		logger.info("Controller:New author save. idAuthor="+author.getIdAuthor());
-		String operationStatus="ok";
-		return operationStatus;
-
-		//	String operationStatus="";
-		//	return operationStatus;
-	}
-
-
-
-
-
-
-
-
-
-
-
-
+//
+//	@Inject
+//	private IAuthorService authorService;
+//
+//	private static final Logger logger = LoggerFactory.getLogger(AuthorController.class);
+//
+//	@RequestMapping(value="/signup", method=RequestMethod.GET)
+//	public ModelAndView signup(ModelMap model) {			
+//		return new ModelAndView();
+//	}
+//
+//	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+//	public @ResponseBody String operationStatus(@RequestBody Author author) {
+//
+//		authorService.addAuthor(author);
+//		logger.info("Controller:New author save. idAuthor="+author.getIdAuthor());
+//		String operationStatus="ok";
+//		return operationStatus;
+//
+//		//	String operationStatus="";
+//		//	return operationStatus;
+//	}
 
 }
