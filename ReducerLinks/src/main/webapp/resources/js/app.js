4f17ebddfc607fb65ab6cbe0d4c24e9a7e5a32f5
Zaerko_DV <zaerko1991@gmail.com>
2015-10-15 11:16:02 +0200

    Add index page

---
+++ ReducerLinks/src/main/webapp/resources/js/app.js
@@ -0,0 +1,68 @@
+var ReducerLinks = {};
+
+var App = angular.module('ReducerLinks', ['ReducerLinks.filters', 'ReducerLinks.services', 'ReducerLinks.directives']);
+App.config(['$routeProvider', function($routeProvider) {
+
+	$routeProvider.when('/index', {
+		 templateUrl: '/index',
+		 controller: 'IndexController'
+	 });
+	
+	 $routeProvider.when('/link', {
+		 templateUrl: 'link/links',
+		 controller: 'LinkController'
+	 });
+	 
+	 $routeProvider.when('/user/signUp', {
+		 templateUrl: 'user/signUp',
+		 controller: 'UserController'
+	 });
+	 
+	 $routeProvider.when('/user/signIn', {
+		    templateUrl: 'user/signIn',
+		    controller: 'UserController'
+	 });
+	 
+    $routeProvider.otherwise({
+    	redirectTo: '/index'
+    });
+}]);
+
+
+//var App={};
+//var App = angular.module('App',[]);
+//
+//App.config(['$routeProvider', '$locationProvider', function($routeProvider) {
+//
+//    $routeProvider.when('/link', {
+//        templateUrl: 'link/startPage',
+//        controller: LinkController
+//    });
+//    
+//    $routeProvider.when('/author/signup', {
+//        templateUrl: 'author/signup',
+//        controller: AuthorController
+//    });
+//
+//    $routeProvider.otherwise({redirectTo: '/link'});
+//}]);
+//
+////Author signin or signup validation
+//App.run(function($rootScope, $location) {
+//	
+//    	// register listener to watch route changes
+//		$rootScope.$on("$routeChangeStart", function(event, next, current) {
+//        console.log("Operation status="+$rootScope.operationStatus);
+//
+//        //operationStatus-this is result of operation which set after each cheak request(object)
+//        if ($rootScope.operationStatus == '' || $rootScope.operationStatus == null) {
+//            // no logged user, we should be going to #login
+//            if (next.templateUrl == "author/failure.jsp") {
+//                // already going to failure, no redirect needed
+//            } else {
+//                // not going to failure, we should redirect now
+//                $location.path("/author/failure.jsp");
+//            }
+//        }
+//    });
+//});
